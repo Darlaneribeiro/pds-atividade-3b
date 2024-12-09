83a909440e169ad87fa40d1da9f0aa32010f0d78
Darlane Ribeiro e Evelyn Maria <Darlane.Evelyn@email.com>
2024-12-09 17:55:34 -0300

    Projeto Pronto!

---
+++ Atividade/src/pds/App.java
@@ -0,0 +1,14 @@
+package pds;
+
+public class App {
+	
+	    public static void main(String[] args) {
+	        // Criando uma instância de Pessoa para cada estuantes
+	        Pessoa pessoa = new Pessoa("Darlane", 19);
+
+	        // Exibindo as informações dos estudantes
+	        pessoa.exibirInformacoes();
+	    }
+	}
+
+
